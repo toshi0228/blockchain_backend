6f6a3192bdcf6db1f231982cd2c0852262ad9701
toshi0228 <etoshi0228@gmail.com>
2022-09-19 19:27:28 +0900

    sqlx導入に伴いリファクタリング

---
+++ src/infra/datamodel/user_datamodel.go
@@ -0,0 +1,18 @@
+package datamodel
+
+import "time"
+
+type User struct {
+	Id        uint32    `db:"id"`
+	Name      string    `db:"name"`
+	Password  string    `db:"password"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+
+	// relationされたwallet情報
+	WalletId          uint32    `db:"wallet_id"`
+	UserID            uint32    `db:"user_id"`
+	BlockchainAddress string    `db:"blockchain_address"`
+	WalletCreatedAt   time.Time `db:"wallet_created_at"`
+	WalletUpdatedAt   time.Time `db:"wallet_updated_at"`
+}
--- src/interface/database/user_repository.go
+++ src/interface/database/user_repository.go
@@ -4,11 +4,11 @@ import (
 	_ "embed"
 	"fmt"
 	"github.com/toshi0228/blockchain/src/entity"
+	"github.com/toshi0228/blockchain/src/infra/datamodel"
 	"github.com/toshi0228/blockchain/src/infra/db"
 	"github.com/toshi0228/blockchain/src/infra/dbtable"
 	"github.com/toshi0228/blockchain/src/usecase/userusecase/input"
 	"log"
-	"time"
 )
 
 type UserRepositoryImpl struct{}
@@ -53,72 +53,48 @@ func (repo UserRepositoryImpl) CreateUser(in *input.CreateUserInput) (*entity.Us
 //go:embed user_repository_find_all.sql
 var findAllUserSql string
 
-type UserRow struct {
-	Id        uint32    `db:"id"`
-	Name      string    `db:"name"`
-	Password  string    `db:"password"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
-
-	// relationされたwallet情報
-	WalletId          uint32    `db:"wallet_id"`
-	UserID            uint32    `db:"user_id"`
-	BlockchainAddress string    `db:"blockchain_address"`
-	WalletCreatedAt   time.Time `db:"wallet_created_at"`
-	WalletUpdatedAt   time.Time `db:"wallet_updated_at"`
-}
-
 // FindAll ユーザーの一覧取得
 func (repo *UserRepositoryImpl) FindAll() ([]*entity.UserPager, error) {
-
-	//customers := make([]userRow, 0)
 
 	rows, err := db.Conn().Queryx(findAllUserSql)
 	if err != nil {
 		return nil, err
 	}
-	//defer rows.C()
 
 	if rows == nil {
 		return nil, nil
 	}
 
 	var users []*entity.UserPager
+
 	for rows.Next() {
 
-		ur := &UserRow{}
-
-		err := rows.StructScan(ur)
+		u := &datamodel.User{}
+		err := rows.StructScan(u)
 
 		if err != nil {
-			fmt.Println("エラーぶん")
+			fmt.Println("エラー文")
 			fmt.Println(err)
 		}
 
-		u, err := entity.NewUserPager(
-			ur.Id,
-			ur.Name,
-			ur.Password,
-			ur.CreatedAt,
-			ur.UpdatedAt,
-			ur.WalletId,
-			ur.UserID,
-			ur.BlockchainAddress,
-			ur.WalletCreatedAt,
-			ur.WalletUpdatedAt,
+		userEntity, err := entity.NewUserPager(
+			u.Id,
+			u.Name,
+			u.Password,
+			u.CreatedAt,
+			u.UpdatedAt,
+			u.WalletId,
+			u.UserID,
+			u.BlockchainAddress,
+			u.WalletCreatedAt,
+			u.WalletUpdatedAt,
 		)
 		if err != nil {
 			return nil, fmt.Errorf(err.Error())
 		}
 
-		log.Println("=============")
-		log.Println(u.Id())
-		log.Println(u.Wallet().BlockchainAddress())
-		log.Println(u.Wallet().Id())
-		log.Println(u.Wallet().UpdatedAt())
-		users = append(users, u)
+		users = append(users, userEntity)
 	}
 
 	return users, nil
-	//return users, nil
 }
