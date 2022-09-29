7e9982cadcdbcfcaa2d64b8366a7317c8dfb4dde
toshi0228 <etoshi0228@gmail.com>
2022-09-29 19:14:18 +0900

    tx のjsonの値を修正

--- src/usecase/transactionusecase/output/get_transaction_list.go
+++ src/usecase/transactionusecase/output/get_transaction_list.go
@@ -3,14 +3,14 @@ package output
 import "time"
 
 type Transactions struct {
-	TransactionList []*Transaction `json:"transaction"`
+	TransactionList []*Transaction `json:"transactions"`
 }
 
 type Transaction struct {
 	Id               uint32    `json:"id"`
-	SenderAddress    string    `json:"sender_address"`
-	RecipientAddress string    `json:"recipient_address"`
+	SenderAddress    string    `json:"senderAddress"`
+	RecipientAddress string    `json:"recipientAddress"`
 	Amount           uint64    `json:"amount"`
-	CreatedAt        time.Time `json:"created_at"`
-	UpdatedAt        time.Time `json:"updated_at"`
+	CreatedAt        time.Time `json:"createdAt"`
+	UpdatedAt        time.Time `json:"updatedAt"`
 }
